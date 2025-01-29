e1c4b228afb229db3fd5e716b429901343a422d4
Carlos Eduardo Galván Salinas <carlosgalvans1999@gmail.com>
2025-01-29 17:27:29 -0600

    Corrección ejercicio 2

--- EjercicioDos_NumeroMayor/src/NumeroMayor.java
+++ EjercicioDos_NumeroMayor/src/NumeroMayor.java
@@ -8,18 +8,18 @@ public class NumeroMayor {
 		int longitudLista = 5;
 		ArrayList<Double> listaNumeros = new ArrayList<>();
 		Double numeroMayor = 0.0;
-		 DecimalFormat df = new DecimalFormat("#.##");
-
+		DecimalFormat df = new DecimalFormat("#.##");
+		// Se crea una lista con 5 números aleatorios Double de dos decimales
 		for (int i = 0; i < longitudLista; i++) {
 			double numero = ThreadLocalRandom.current().nextDouble(1.0, 100.0);
 			double numeroRedondeado = Double.parseDouble(df.format(numero));
 			listaNumeros.add(numeroRedondeado);
 		}
 		for (int i = 0; i < 1; i++) {
-			for (int j = 1; j < listaNumeros.size() - 1; j++) {
+			for (int j = 1; j < listaNumeros.size(); j++) {
 				if (listaNumeros.get(i) > listaNumeros.get(j) && listaNumeros.get(i) > numeroMayor) {
 					numeroMayor = listaNumeros.get(i);
-				} else if(listaNumeros.get(j) > numeroMayor) {
+				} else if (listaNumeros.get(j) > numeroMayor) {
 					numeroMayor = listaNumeros.get(j);
 				}
 			}
